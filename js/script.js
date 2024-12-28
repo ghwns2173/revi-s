17b41cf6848c6b8cf56648a29c4779399542ae6b
ghwns2173 <ghwjd2173@naver.com>
2024-12-28 12:42:52 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+document.addEventListener(`DOMContentLoaded`, function () {
+  AOS.init();
+
+  
+  var swiper = new Swiper(".bannerSwiper", {
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    //   pagination: {
+    //     el: ".swiper-pagination",
+    // },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+  });
+  
+  var swiper = new Swiper(".shopSwiper", {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: true,
+          },
+  });
+
+  // -------------------------------------------------
+  
+  function toggleAOS() {
+      const screenWidth = window.innerWidth;
+
+      if (screenWidth < 960) {
+        // 960px 미만에서는 AOS 비활성화
+        AOS.init({ disable: true });
+      } else {
+        // 960px 이상에서는 AOS 활성화
+        AOS.init();
+      }
+    }
+
+    // 초기 실행
+    toggleAOS();
+
+    // 화면 크기 변경 시 실행
+    window.addEventListener('resize', toggleAOS);
+
+
+
+
+}); // end
